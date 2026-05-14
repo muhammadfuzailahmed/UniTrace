98d27f0a03158261dd23bd3a299e1aeefda086b6
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-05-14 16:21:55 +0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace Database_lab_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> names = new List<string>();
+
+            string conStr = "Server=DESKTOP-87911Q0\\SQLEXPRESS;Database=dbms_lab_project;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+
+                string query = "SELECT Name FROM Students";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    names.Add(reader["Name"].ToString());
+                }
+            }
+
+            return View(names);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+
+            string connectionString = "Server=DESKTOP-87911Q0\\SQLEXPRESS;Database=dbms_lab_project;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            using (SqlConnection conn = new SqlConnection(connectionString)) {
+                conn.Open();
+
+                string query = "select email, password from users";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    string dbEmail = reader["email"].ToString();
+                    string dbpassword = reader["password"].ToString();
+
+                    if(username == dbEmail && password == dbpassword)
+                    {
+                        TempData["Message"] = "Login Successful";
+                        TempData["Type"] = "success";
+                        return RedirectToAction("Login");
+                    }
+
+                }
+
+            }
+            TempData["Message"] = "Login Failed";
+            TempData["Type"] = "error";
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string name, string email, string password)
+        {
+            string connectionString = "Server=DESKTOP-87911Q0\\SQLEXPRESS;Database=dbms_lab_project;Trusted_Connection=True;TrustServerCertificate=True;";
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "insert into users (name, email, password) values (@Name, @Email, @Password)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                cmd.ExecuteNonQuery();
+                TempData["Message"] = "Account Created Successfully";
+                TempData["Type"] = "success";
+
+                return RedirectToAction("Login");
+            }
+        }
+
+    }
+}
