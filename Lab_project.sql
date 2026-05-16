bc83f53842da463596fccbfc0d6afbce7f820c48
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-05-16 16:14:58 +0500

    add query to display lost items

--- Lab_project.sql
+++ Lab_project.sql
@@ -17,7 +17,7 @@ insert into users (name, email, password) values ('Fuzail', 'fuzail@gmail.com', 
 create table items (
 	item_id int primary key identity(1,1),
 	title varchar(50),
-	description varchar(120),
+	description varchar(MAX),
 	category varchar(40),
 	lost_date date,
 	location_found varchar(80),
@@ -25,3 +25,5 @@ create table items (
 	id int foreign key references users(id),
 	requesting_user int foreign key references users(id)
 )
+
+select * from items where id = 2
