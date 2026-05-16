73c6a54a15d6fe11f98767180c381783e90479a6
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-05-16 16:12:56 +0500

    add item list

---
+++ Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Database_lab_project.Models
+{
+    public class Item
+    {
+        public int itemId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime LostDate { get; set; }
+        public string LocationFound { get; set; }
+
+        public string is_found { get; set; }
+
+        public int userId { get; set; }
+
+        public int requestingUser { get; set; }
+
+    }
+}
